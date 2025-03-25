412d38b08cffc688c616eeea672b5148d0714c36
czlabinger <czlabinger@student.tgm.ac.at>
2025-03-25 22:02:59 +0100

    Updates

---
+++ precache-manifest.17926379bde6f9f412f7ff2fd202d629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93438d7701e47b529f948cc5e329e3c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c723cc0dbc7f0d7c7d1",
+    "url": "/static/css/main.dd595524.chunk.css"
+  },
+  {
+    "revision": "9e7701a68e3c6f906670",
+    "url": "/static/js/2.b0433acf.chunk.js"
+  },
+  {
+    "revision": "ab790d7f4927e929140961c444db2a72",
+    "url": "/static/js/2.b0433acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c723cc0dbc7f0d7c7d1",
+    "url": "/static/js/main.49294b97.chunk.js"
+  },
+  {
+    "revision": "84eb67ca77b547b420c3",
+    "url": "/static/js/runtime-main.df7a09ad.js"
+  }
+]);
