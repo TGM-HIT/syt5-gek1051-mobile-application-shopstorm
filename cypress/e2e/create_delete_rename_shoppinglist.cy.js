3a309b83686880774c7074e14c7e93a0990b2752
pbakshi05 <pbakshi@student.tgm.ac.at>
2025-03-31 23:41:31 +0200

    Testfälle für shoppingliste bearbeiten,erstellen und löschen

---
+++ cypress/e2e/create_delete_rename_shoppinglist.cy.js
@@ -0,0 +1,46 @@
+beforeEach(() => {
+  cy.visit('http://localhost:3000')
+  cy.window().should('have.property', 'document').and('have.property', 'readyState', 'complete');
+  cy.wait(2000);
+
+})
+
+describe('Create ShoppingList', () => {
+  it('passes', () => {
+
+    cy.get('#add').click();
+    cy.get('.MuiTextField-root').type("Meine Shoppingliste{enter}");
+    })
+})
+
+describe('Open ShoppingList', () => {
+  it('passes', () => {
+
+    cy.get('#add').click();
+    cy.get('.MuiTextField-root').type("Meine Shoppingliste{enter}");
+    cy.get('.MuiButtonBase-root').eq(2).click();
+    cy.get('[role="menuitem"]').eq(0).click();
+  })
+})
+
+describe('Rename ShoppingList', () => {
+  it('passes', () => {
+
+    cy.get('#add').click();
+    cy.get('.MuiTextField-root').type("Meine Shoppingliste{enter}");
+    cy.get('.MuiButtonBase-root').eq(2).click();
+    cy.get('[role="menuitem"]').eq(1).click();
+  })
+})
+
+
+describe('Delete ShoppingList', () => {
+  it('passes', () => {
+
+    cy.get('#add').click();
+    cy.get('.MuiTextField-root').type("Meine Shoppingliste{enter}");
+    cy.get('.MuiButtonBase-root').eq(2).click();
+    cy.get('[role="menuitem"]').eq(2).click();
+
+  })
+})
